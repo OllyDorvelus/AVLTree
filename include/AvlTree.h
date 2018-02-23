9b97956524d25d4086e1666eff0f98cce09e09f2
OllyDorvelus <ollydorvelus@gmail.com>
2018-02-22 22:48:47 -0500

    initial commit

---
+++ include/AvlTree.h
@@ -0,0 +1,16 @@
+#ifndef AVLTREE_H
+#define AVLTREE_H
+
+
+class AvlTree
+{
+    public:
+        AvlTree();
+        virtual ~AvlTree();
+
+    protected:
+
+    private:
+};
+
+#endif // AVLTREE_H
